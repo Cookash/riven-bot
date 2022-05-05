45df593af7b1fb968a609cce4433bb33f590a144
Cookash <xmistrz1x@gmail.com>
2022-05-05 12:57:30 +0200

    RDB testing Jira

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ for (const file of commandFiles) {
 }
 
 client.once('ready', () => {
-	console.log('Ready!');
+	console.log('Bot is ready!');
 });
 
 client.on('interactionCreate', async interaction => {
